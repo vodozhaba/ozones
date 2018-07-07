538fbe0c1fb23d579846d6571bbf79b601d93fb8
vodozhaba <vodozhaba@vodozhaba.net>
2018-07-08 00:13:58 +0200

    implement the ALU block

--- cpu.cpp
+++ cpu.cpp
@@ -18,7 +18,40 @@ namespace ozones {
         switch(instruction.GetMnemonic()) {
         case Instruction::kOra:
             reg_a_ |= OperandRead(instruction.GetOperand(0));
+            UpdateStatus(reg_a_);
             break;
+        case Instruction::kAnd:
+            reg_a_ &= OperandRead(instruction.GetOperand(0));
+            UpdateStatus(reg_a_);
+            break;
+        case Instruction::kEor: // Seriously? You don't know the word XOR?
+            reg_a_ ^= OperandRead(instruction.GetOperand(0));
+            UpdateStatus(reg_a_);
+            break;
+        case Instruction::kAdc: {
+            uint8_t operand = OperandRead(instruction.GetOperand(0));
+            Adc(operand);
+            break;
+        }
+        case Instruction::kSta:
+            OperandWrite(instruction.GetOperand(0), reg_a_);
+            break;
+        case Instruction::kLda:
+            reg_a_ = OperandRead(instruction.GetOperand(0));
+            break;
+        case Instruction::kCmp: {
+            uint8_t operand = OperandRead(instruction.GetOperand(0));
+            SetFlag(kNegative, reg_a_ < operand);
+            SetFlag(kZero, reg_a_ == operand);
+            SetFlag(kCarry, reg_a_ > operand);
+            break;
+        }
+        case Instruction::kSbc: {
+            // A + M = A + (-M)
+            uint8_t operand = OperandRead(instruction.GetOperand(0));
+            Adc(~operand);
+            break;
+        }
         default:
             std::stringstream ss;
             ss << "Unknown instruction: " << instruction.GetMnemonic();
@@ -33,14 +66,14 @@ namespace ozones {
         case Operand::kAbsolute:
             return ram_->ReadByte(operand.GetValue());
         case Operand::kImplied:
-            throw new std::runtime_error("Attempted to read implied operand");
+            throw new std::runtime_error("Attempted to read an implied operand");
         case Operand::kIndirectAbsolute:
             return ram_->ReadWord(operand.GetValue());
         case Operand::kAbsoluteIndexedX:
             return ram_->ReadByte(operand.GetValue() + reg_x_);
         case Operand::kAbsoluteIndexedY:
             return ram_->ReadByte(operand.GetValue() + reg_y_);
-            // Spot the difference
+        // Spot the difference
         case Operand::kIndexedIndirect:
             return ram_->ReadByte(ram_->ReadWord(operand.GetValue() + reg_x_));
         case Operand::kIndirectIndexed:
@@ -49,10 +82,71 @@ namespace ozones {
             return reg_pc_ + (int8_t) operand.GetValue();
         case Operand::kAccumulator:
             return reg_a_;
+        default:
+            std::stringstream ss;
+            ss << "Unknown addressing mode: " << operand.GetMode();
+            throw std::runtime_error(ss.str());
         }
-        std::stringstream ss;
-        ss << "Unknown addressing mode: " << operand.GetMode();
-        throw std::runtime_error(ss.str());
     }
+
+    void Cpu::OperandWrite(Operand operand, uint8_t value) {
+        switch(operand.GetMode()) {
+        case Operand::kImmediate:
+            throw new std::runtime_error("Attemped to write to an immediate value");
+        case Operand::kAbsolute:
+            ram_->WriteByte(operand.GetValue(), value);
+            break;
+        case Operand::kImplied:
+            throw new std::runtime_error("Attempted to write to an implied operand");
+        case Operand::kIndirectAbsolute:
+            throw new std::runtime_error("Attempted to write to an indirect absolute operand");
+        case Operand::kAbsoluteIndexedX:
+            ram_->WriteByte(operand.GetValue() + reg_x_, value);
+            break;
+        case Operand::kAbsoluteIndexedY:
+            ram_->WriteByte(operand.GetValue() + reg_y_, value);
+            break;
+        // Spot the difference
+        case Operand::kIndexedIndirect:
+            ram_->WriteByte(ram_->ReadWord(operand.GetValue() + reg_x_), value);
+            break;
+        case Operand::kIndirectIndexed:
+            ram_->WriteByte(ram_->ReadWord(operand.GetValue()) + reg_y_, value);
+            break;
+        case Operand::kRelative:
+            throw new std::runtime_error("Attempted to write to a relative operand");
+        case Operand::kAccumulator:
+            reg_a_ = value;
+            break;
+        default:
+            std::stringstream ss;
+            ss << "Unknown addressing mode: " << operand.GetMode();
+            throw std::runtime_error(ss.str());
+        }
+    }
+
+    void Cpu::UpdateStatus(uint8_t result) {
+        SetFlag(kZero, result == 0x00);
+        SetFlag(kNegative, result & 0x80);
+    }
+
+    void Cpu::SetFlag(StatusFlag flag, bool value) {
+        if(value)
+            reg_sp_ |= flag;
+        else
+            reg_sp_ &= ~flag;
+    }
+
+    void Cpu::Adc(uint8_t operand) {
+        uint8_t old_a = reg_a_;
+        reg_a_ += operand;
+        if(reg_status_ | kCarry)
+            ++reg_a_;
+        UpdateStatus(reg_a_);
+        SetFlag(kCarry, old_a < reg_a_);
+        // If the operands have the same sign and the result has a different one
+        SetFlag(kOverflow, ((old_a & 0x80) == (operand & 0x80)) && ((old_a & 0x80) != (reg_a_ & 0x80)));
+    }
+
 
 }
--- cpu.h
+++ cpu.h
@@ -12,9 +12,23 @@ namespace ozones {
         Cpu(std::shared_ptr<Ram> ram);
         void Tick();
     private:
+        enum StatusFlag {
+            kCarry              = 0x01,
+            kZero               = 0x02,
+            kInterruptDisable   = 0x04,
+            kDecimalMode        = 0x08,
+            kBrkOrPhp           = 0x10,
+            kAlwaysSet          = 0x20,
+            kOverflow           = 0x40,
+            kNegative           = 0x80
+        };
         uint8_t reg_a_, reg_x_, reg_y_, reg_sp_, reg_pc_, reg_status_;
         std::shared_ptr<Ram> ram_;
         void ExecuteInstruction(Instruction instruction);
         uint16_t OperandRead(Operand operand);
+        void OperandWrite(Operand operand, uint8_t value);
+        void UpdateStatus(uint8_t result);
+        void SetFlag(StatusFlag flag, bool value);
+        void Adc(uint8_t operand);
     };
 }
